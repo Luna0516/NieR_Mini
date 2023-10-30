c75e9d8837667bd9274b06a79f392c407096a0b9
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2023-10-30 10:18:35 +0900

    플레이어 이동 구현
    적을 만들고 가장 가까운 적을 먼저 조준하게끔
    회전값 주는거로 적을 만들고 생각하기...

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float moveSpeed = 3.0f;
+
+    Vector3 moveDir;
+
+    Vector3 nextPos;
+
+    Rigidbody rigid;
+
+    PlayerInputActions inputActions;
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+
+        inputActions = new PlayerInputActions();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    // <Fuc> ==========================================================================
+
+    private void Move()
+    {
+        nextPos = Time.fixedDeltaTime * moveSpeed * moveDir;
+
+        rigid.MovePosition(rigid.position + nextPos);
+    }
+
+    // <InputAction> ==========================================================================
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 inputDir = context.ReadValue<Vector2>();
+
+        moveDir.x = inputDir.x;
+        moveDir.y = 0;
+        moveDir.z = inputDir.y;
     }
 }
